c3bf9f4c6f0f48d35082631a3710f594ac20087b
Colton Loewen <loewencjay@outlook.com>
2021-06-07 21:40:30 -0600

    Added resource pack alternative
    Was unaware the Fabric API is needed to create a resource pack from the mod. Added an alternative.

--- src/main/java/ca/cjloewen/snowedin/mixin/GameRulesMixin.java
+++ src/main/java/ca/cjloewen/snowedin/mixin/GameRulesMixin.java
@@ -3,7 +3,6 @@ package ca.cjloewen.snowedin.mixin;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
--- src/main/java/ca/cjloewen/snowedin/mixin/WeatherCommandMixin.java
+++ src/main/java/ca/cjloewen/snowedin/mixin/WeatherCommandMixin.java
@@ -2,14 +2,19 @@ package ca.cjloewen.snowedin.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.command.WeatherCommand;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.TranslatableText;
 
 @Mixin(WeatherCommand.class)
 public class WeatherCommandMixin {
 	private static int executeClear(ServerCommandSource source, int duration) {
-		source.sendError(new TranslatableText("commands.weather.set.snowedin"));
+		if (FabricLoader.getInstance().isModLoaded("fabric"))
+			source.sendError(new TranslatableText("commands.weather.set.snowedin"));
+		else
+			source.sendError(new LiteralText("There is no escape from the snow."));
 		return duration;
 	}
 }
